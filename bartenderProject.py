bf04ea5b59cb00d581a79ca20be79eaf8ab7e06f
Tuan-Vu Duc <vuductuan01031997@gmail.com>
2021-03-21 14:28:48 +0700

    reup

--- bartenderProject.py
+++ bartenderProject.py
@@ -21,42 +21,23 @@ ingredients = {
 def drinkRadum(_drink):
     return random.choice(_drink)
 
-def query_yes_no(question, default="yes"):
-    """Ask a yes/no question via raw_input() and return their answer.
-
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
-
-    The "answer" return value is True for "yes" or False for "no".
-    """
-    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
+def answersQues(_inString):
+    print(_inString + " [y/n] ")
     while True:
-        sys.stdout.write(question + prompt)
-        choice = input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
+        _input = input()
+        if (_input.upper() == 'YES') | (_input.upper() == 'Y'):
+            return True
+        elif (_input.upper() == 'NO') | (_input.upper() == 'N'):
+            return False
         else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "
-                             "(or 'y' or 'n').\n")
+            print("Please respond with 'yes' or 'no' or 'y' or 'n'.")
+            continue
 
 # Run
 print("Nice to meet you, pls choose your drink!")
 drinkOder = ""
 for drink in questions:
-    if query_yes_no(questions[drink]):
+    if answersQues(questions[drink]):
         _choice = drinkRadum(ingredients[drink])
         print("i choice",_choice," for you!")
         drinkOder = drinkOder + _choice + " and "
